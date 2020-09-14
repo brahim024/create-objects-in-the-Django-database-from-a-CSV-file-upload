a78164679849219444ccf69d2aad28a15a15b654
brahim024 <ifninos168@gmail.com>
2020-09-14 02:29:20 +0200

    add save abd calcul pruduct price

--- sales/models.py
+++ sales/models.py
@@ -1,3 +1,31 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+PRODUCT_CHOICES=(
+	('TV','tv'),
+	('IPAD','ipad'),
+	('PLAYSTATION','playstation'),
+	)
+class Sale(models.Model):
+	product=models.CharField(max_length=20,choices=PRODUCT_CHOICES)
+	seleman=models.ForeignKey(User,on_delete=models.CASCADE)
+	quantity=models.PositiveIntegerField()
+	total=models.FloatField(blank=True)
+	created_date=models.DateTimeField(auto_now=True)
+	def __str__(self):
+		return f"{self.product}-{self.quantity}"
+
+	def save(self,*args,**kwargs):
+		price=None
+		if self.product=='TV':
+			price=558,93
+		elif self.product=='IPAD':
+			price=298,23
+		elif self.product=='PLAYSTATION':
+			price=450,83
+		else:
+			pass
+		self.total=price * self.quantity
+		super().save(*args,**kwargs)
+
+
